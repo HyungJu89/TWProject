7f056cfbae9da4b6387ccfca6ac5d6aa6d5fc902
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-07-05 20:05:22 +0900

    [최지은] > 상단 바 CSS 작업 시작

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/basic/Basic.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/basic/Basic.js
@@ -2,11 +2,14 @@
 // ^워링 업애주는 친구
 
 import React from 'react';
+import styles from './style/Basic.module.css'; 
 
 function Basic() {
     return (
-        <div>
-            기본상단입니다.
+        <div className={styles.basicNav}>
+            <div>쥐구멍</div>
+            <div><input/></div>
+            <div>로그인</div>
         </div>
     );
 }
