d8d566f3cdc921d1c57dc1f7e3d633e3853b94dd
ImPark <jaeyeol0311@gmail.com>
2024-07-06 15:58:07 +0900

    치지직 스트리밍 IMG API 완료

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/channel/MainBanner.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/channel/MainBanner.js
@@ -1,18 +1,27 @@
+import './style/MainBanner.module.css';
+
 
 function MainBanner({ channelId }){
 
 
-    let channe = useQuery('channe', () =>
-        axios.get('https://api.chzzk.naver.com/service/v1/channels/' + channelId).then((a) =>
-            a.data
-        )
-    )
+    // let channe = useQuery('channe', () =>
+    //     axios.get('https://api.chzzk.naver.com/service/v1/channels/'+channelId+'/data?fields=topExposedVideos').then((a) =>
+    //         a.data
+    //     )
+    // )
 
+    let channe = useQuery('channe', () =>
+    axios.get('https://api.chzzk.naver.com/service/v1/channels/b5ed5db484d04faf4d150aedd362f34b/data?fields=topExposedVideos').then((a) =>
+        a.data
+    )
+)
+
+    let liveImageUrl = (channe.data.content.topExposedVideos.liveImageUrl("{type}", "480"));
 
 
     return(
         <div className="위치 고정">
-            <img src={channe.data.content.channelImageUrl} className="이미지 사이즈 조정 및 다른 div에 영향을 주지않게"/>
+            <img src={liveImageUrl} className="liveImage"/>
             <div className="맨위에 비어있는칸"></div>
             <div className="라이브 정보">
                 <div className="라이브 아이콘"></div>
