f0738b51d311a95513a7af57e50267e35e00db22
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-07-06 15:58:02 +0900

    [최지은] > 상단 바 CSS 제작

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/basic/Basic.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/basic/Basic.js
@@ -3,15 +3,30 @@
 
 import React from 'react';
 import styles from './style/Basic.module.css'; 
+import '../App.css'; 
 
 function Basic() {
+let loginOn = true;
+
     return (
         <div className={styles.basicNav}>
-            <div>쥐구멍</div>
-            <div><input/></div>
-            <div>로그인</div>
+            <div className={styles.divWidth}>쥐구멍</div>
+            <div className={styles.divWidth}><input placeholder='검색어를 입력하세요'/></div>
+            <div className={styles.icon}>
+            { loginOn === true && <Icon/> }
+            <div className={styles.signUpBtn}>로그인</div>
+            </div>
         </div>
     );
+}
+
+function Icon(){
+    return(
+    <>
+    <div>★</div>
+    <div>★</div>
+    </>
+    )
 }
 
 export default Basic;
