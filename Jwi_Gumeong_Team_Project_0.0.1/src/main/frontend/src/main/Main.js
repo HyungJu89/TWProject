2f6a258c3e26a5c7ba9fcf5133ab61a97b70c383
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-07-11 16:51:39 +0900

    [최지은] > App.css 버튼 기본 프리셋 제작

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/main/Main.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/main/Main.js
@@ -8,9 +8,7 @@ function Main() {
     let navigate = useNavigate();
     return (
         <div>
-            최지은fasfasfasfa
-            <button onClick={()=>{navigate('/channel/'+'123')}}>채널 확인 URL</button>
-            <div style={{backgroundImage : 'url(https://codingapple1.github.io/shop/shoes1.jpg)'}}></div>
+            <button style={{width : '150px', height : '50px'}} onClick={()=>{navigate('/channel/'+'123')}}>채널 확인 URL</button>
         </div>
     );
 }
