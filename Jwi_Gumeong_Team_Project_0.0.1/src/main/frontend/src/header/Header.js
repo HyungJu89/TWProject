07390f5097d2a281ee7a155682a0d25cf9d0af63
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-07-11 18:31:36 +0900

    [최지은] > 검색 모달창 1차 완성

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/header/Header.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/header/Header.js
@@ -5,14 +5,15 @@ import React, { useEffect, useState } from 'react';
 import styles from './style/Header.module.css';
 //이미지 import
 import searching from '../icon/24px/searching.png';
+import deletion from '../icon/14px/deletion.png';
 import Open_channel from '../icon/24px/Open-channel.png';
 import notification_activation from '../icon/24px/notification-activation.png';
 import notification_deactivation from '../icon/24px/notification-deactivation.png';
 import '../App.css';
 
 function Header() {
-    let [loginOn, setLoginOn] = useState(true);
-    let [justSearchOn, setJustSearchOn] = useState(false);
+    let [loginOn, setLoginOn] = useState(true); //로그인 확인 변수
+    let [justSearchOn, setJustSearchOn] = useState(true); //검색창 클릭시 노출되는 모달창 확인
     let [notification_img, setNotification_deactivation] = useState(notification_deactivation)
 
     useEffect(() => {
@@ -40,7 +41,7 @@ function Header() {
     );
 }
 
-function Icon() { /* 로그인 상황에 따른 모달 UI */
+function Icon() { /* 로그인 시 노출되는 알림 아이콘 UI */
     return (
         <>
             <div><img src={Open_channel} /></div>
@@ -51,7 +52,7 @@ function Icon() { /* 로그인 상황에 따른 모달 UI */
         </>
     )
 }
-function JustSearch() {
+function JustSearch() { /* 최근 검색어 모달창 */
     useEffect(() => {
         {/* 최근검색어 미완성 */ }
         let justSearchLocal = localStorage.getItem('search') //최근검색어 로컬스토리지 생성
@@ -62,29 +63,19 @@ function JustSearch() {
         localStorage.setItem('search', JSON.stringify(justSearchLocal))
     }, [])
 
-    let [justSearchOn, setJustSearchOn] = useState(true) //나중에... 최근 검색어 on/off로 바꿔야함
+    let [임시, set임시] = useState(true) //나중에... 최근 검색어 on/off로 바꿔야함
 
     return (
-        <div className={styles.JustSearchBase}>
-            <h4>최근검색어</h4>
-            {justSearchOn === true ?
+    <div className={styles.JustSearchBase}><h4>최근검색어</h4>
+            {임시 === true ?
                 <div className={styles.mainDiv}>
                     <div className={styles.list}>
-                        <div><img src={searching} />요즘 검색어</div>
-                        <div><img src={searching} />요즘 검색어</div>
-                        <div><img src={searching} />요즘 검색어</div>
-                        <div><img src={searching} />요즘 검색어</div>
-                        <div><img src={searching} />요즘 검색어</div>
-                        <div><img src={searching} />요즘 검색어</div>
-                        <div><img src={searching} />요즘 검색어</div>
-                        <div><img src={searching} />요즘 검색어</div>
-                        <div><img src={searching} />요즘 검색어</div>
-                        <div><img src={searching} />요즘 검색어</div>
-                        <div><img src={searching} />요즘 검색어</div>
-                    </div>
-                </div>
+                        <div><img src={searching} /> {/* 서치아이콘 */}
+                        <p>1saf</p>
+                        <img style={{height:'14px'}} src={deletion} /></div> {/* 삭제아이콘 */}
+                    </div></div>
                 : <p>최근 검색 내역이 없어요.</p>}
-        </div>
+    </div>
     )
 }
 export default Header;
--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/main/Main.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/main/Main.js
@@ -14,3 +14,4 @@ function Main() {
 }
 
 export default Main;
+ 
