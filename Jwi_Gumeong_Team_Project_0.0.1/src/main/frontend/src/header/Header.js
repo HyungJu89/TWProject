72978e9ae5ff0c56e2c8b3b173bb505d72f0a41e
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-07-11 19:40:05 +0900

    [최지은] > Header 완성 (ps. 로컬히스토리지 미연결)

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/header/Header.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/header/Header.js
@@ -1,8 +1,13 @@
+/* TODO
+    - 로컬히스토리 연결 (근데 이건 채널 완성되고 해도 될듯)
+*/
+
 /* eslint-disable */
 // ^워링 업애주는 친구
 
 import React, { useEffect, useState } from 'react';
 import styles from './style/Header.module.css';
+import {Link} from 'react-router-dom';
 //이미지 import
 import Logo from '../icon/logo/logo.png'; //로고 이미지
 import searching from '../icon/24px/searching.png';
@@ -15,8 +20,6 @@ import '../App.css';
 function Header() {
     let [loginOn, setLoginOn] = useState(true); //로그인 확인 변수
     let [justSearchOn, setJustSearchOn] = useState(false); //검색창 클릭시 노출되는 모달창 확인
-    let [notification_img, setNotification_deactivation] = useState(notification_deactivation)
-
     useEffect(() => {
         {/* 최근검색어 미완성 */ }
         let justSearchLocal = localStorage.getItem('search')
@@ -28,29 +31,30 @@ function Header() {
     return (
         <>
             <div className={styles.basicNav}>
-                <div className={styles.divWidth}> <img src={Logo} /> </div>
+                <div className={styles.divWidth}><Link to="/"><img src={Logo} /></Link></div>
                 <div className={styles.divWidth}>
                     <input onClick={() => { setJustSearchOn(true) }}  onBlur={()=>{setJustSearchOn(false)}} placeholder='검색어를 입력하세요' /></div>
                 {justSearchOn == true ? <JustSearch /> : null} {/* 최근 검색 모달*/}
                 <div className={styles.icon}>
-                    {loginOn === true && <Icon />} {/* 로그인 체크 */}
+                    {loginOn === true && <Icon/>} {/* 로그인 체크 */}
                     <div className={styles.signUpBtn}>로그인</div>
                 </div>
             </div>
         </>
     );
 }
-
 function Icon() { /* 로그인 시 노출되는 알림 아이콘 UI */
+let [img, setImg] = useState(notification_deactivation) /*알림 hover 이팩트 변환용*/
     return (
         <>
-            <div><img src={Open_channel} /></div>
-            <div><img
-                onMouseEnter={() => { src={notification_deactivation} }}
-                onMouseLeave={() => { src={notification_deactivation}  }}
-                src={notification_deactivation} /></div>
+            <div className={styles.icon_notification}>
+                <img src={Open_channel} alt="Open Channel" /></div>
+            <div className={styles.icon_notification}
+                onMouseEnter={() => setImg(notification_activation)}
+                onMouseLeave={() => setImg(notification_deactivation)}>
+                <img src={img} alt="Notification"/></div>
         </>
-    )
+    );
 }
 function JustSearch() { /* 최근 검색어 모달창 */
     useEffect(() => {
@@ -64,15 +68,14 @@ function JustSearch() { /* 최근 검색어 모달창 */
     }, [])
 
     let [임시, set임시] = useState(true) //나중에... 최근 검색어 on/off로 바꿔야함
-
     return (
     <div className={styles.JustSearchBase}><h4>최근검색어</h4>
             {임시 === true ?
                 <div className={styles.mainDiv}>
                     <div className={styles.list}>
-                        <div><img src={searching} /> {/* 서치아이콘 */}
-                        <p>1saf</p>
-                        <img style={{height:'14px'}} src={deletion} /></div> {/* 삭제아이콘 */}
+                    {/* 서치 icon */} <div><img src={searching} /> 
+                    {/* 글내용 */}    <p>1saf</p> 
+                    {/* 삭제 icon */} <img style={{height:'14px', cursor : 'pointer'}} src={deletion} /></div>
                     </div></div>
                 : <p>최근 검색 내역이 없어요.</p>}
     </div>
