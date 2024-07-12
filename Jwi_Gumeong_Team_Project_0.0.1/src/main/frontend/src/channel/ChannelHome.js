ff4e105aeb7d2072cba2ffbcca7ec1f7f3c8ffa0
ImPark <jaeyeol0311@gmail.com>
2024-07-12 18:05:16 +0900

    [임재열] 이미지 정렬

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/channel/ChannelHome.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/channel/ChannelHome.js
@@ -7,9 +7,9 @@ import { useEffect, useState } from 'react';
 
 function ChannelHome() {
     let { id } = useParams();
-    const [imgUrl, setImgUrl] = useState('');
     const [channel, setChannel] = useState('');
     const [liveInfo, setLiveInfo] = useState('');
+
     // 첫 번째 쿼리: 채널 정보를 가져오기.
     const { data: channelApi, isLoading: isLoadingChannel, isError: isErrorChannel } = useQuery('channel', () =>
         axios.get(`/channelRest/search/${id}`)
@@ -55,69 +55,79 @@ function ChannelHome() {
 
     return (
         <div className={style.ChannelHome}>
-            <div>
-
-            {channel.openLive ? (
-                <div className={style.MainBanner}>
-                    <div className={style.liveImage}>
-                        <img src={liveInfo?.liveImageUrl?.replace("{type}", 1080)} alt="Live Image" />
-                        <div className={style.liveInfo}>
-                            <div className={style.liveIcon}>
-                                <div className={style.liveText}>Live</div>
-                            </div>
-                            <div className={style.liveTitle}>
-                                {liveInfo.liveTitle}
-                            </div>
-                            <div className={style.liveGoing} onClick={() => window.open(`https://chzzk.naver.com/live/${id}`)}>
-                                새창으로 방송보기
+            <div className={style.channelInfoBack}>
+                <div>
+                    {channel.openLive ? (
+                        <div className={style.MainBanner}>
+                            <div className={style.liveImage}>
+                                <img src={liveInfo?.liveImageUrl?.replace("{type}", 1080)} alt="Live Image" />
+                                <div className={style.liveInfo}>
+                                    <div className={style.liveIcon}>
+                                        <div className={style.liveText}>Live</div>
+                                    </div>
+                                    <div className={style.liveTitle}>
+                                        {liveInfo.liveTitle}
+                                    </div>
+                                    <div className={style.liveGoing} onClick={() => window.open(`https://chzzk.naver.com/live/${id}`)}>
+                                        새창으로 방송보기
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            ) : (
-                <div className={style.backgroundImage}>라이브 off 배너</div>
-            )}
-            <div className={style.channelBody}>
-                <div className={style.channelInfo}>
-                    <div className={style.iconBack}>
-                        <div className={style.channelIcon}>
-                            <img src={channel.channelImageUrl} alt="Channel Icon" />
-                        </div>
-                    </div>
-                    <div className={style.iconRight}>
-                        <div className={style.imageBackground}>
+                    ) : (
+                        <div className={style.backgroundImage}>라이브 off 배너</div>
+                    )}
+                    <div className={style.channelBody}>
+                        <div className={style.channelInfo}>
+                            <div className={style.iconBack}>
+                                <div className={style.channelIcon}>
+                                    <img className={style.icon} src={channel.channelImageUrl} alt="Channel Icon" />
+                                </div>
+                            </div>
                             <div className={style.iconRight}>
-                                <div className={style.channelTitle}>{channel.channelName}</div>
-                                <div className={style.announcement}>채널 공지사항</div>
+                                <div className={style.imageBackground}>
+                                    <div className={style.iconRight}>
+                                        <div className={style.channelTitle}>{channel.channelName}</div>
+                                        <div className={style.announcement}>채널 공지사항</div>
+                                    </div>
+                                    <div className={style.bookmarkButtonBack}>
+                                        <div className={style.bookmarkButton}>즐겨찾기 버튼</div>
+                                    </div>
+                                </div>
+                                <div className={style.whiteBackground}>
+                                    <div className={style.mark}>
+                                        <div className={style.markText}>팔로워</div>
+                                        <div className={style.markCount}> {channel.followerCount}</div>
+                                    </div>
+                                    <div className={style.mark}>
+                                        <div className={style.markText}>즐겨찾기</div>
+                                        <div className={style.markCount}> 10000</div>
+                                    </div>
+                                </div>
                             </div>
-                            <div className={style.bookmarkButtonBack}>
-                            <div className={style.bookmarkButton}>즐겨찾기 버튼</div>
-                            </div>
-                        </div>
-                        <div className={style.whiteBackground}>
-                            <div className={style.followerCount}>팔로워수: {channel.followerCount}</div>
-                            <div className={style.bookmark}>즐겨찾기</div>
                         </div>
                     </div>
-                </div>
-                <div className={style.mainList}>
-                <div >
-                    <div>글리스트</div>
-                    <div>
-                        <div>
-                            <div></div>
+                    <div className={style.mainList}>
+                        <div >
+                            <div>글리스트</div>
+                            <div>
+                                <div>
+                                    <div></div>
+                                </div>
+                                <div>추천창</div>
+                                <div>사이드 배너</div>
+                            </div>
                         </div>
-                        <div>추천창</div>
-                        <div>사이드 배너</div>
+                        <div className={style.bottomPaging}>페이징</div>
                     </div>
                 </div>
-                <div className={style.bottomPaging}>페이징</div>
-                </div>
+
             </div>
+
         </div>
-        </div>    
+
     );
-    
+
 }
 
 export default ChannelHome;
