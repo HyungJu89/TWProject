0c690cf2f0b8f9daa10f3e5902169436e5e29018
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-07-12 15:35:42 +0900

    [최지은] > 주석추가, PublicBoard.js 추가

---
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/main/PublicBoard.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+// ^워링 업애주는 친구
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './style/Main.module.css';
+import '../App.css';
+
+function PublicBoard() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PublicBoard;
