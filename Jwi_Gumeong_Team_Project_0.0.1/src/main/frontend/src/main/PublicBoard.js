08129772c440418b994c1e37b20289b8004ffcc0
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-07-13 18:23:32 +0900

    [최지은] > 퍼블릭보드 제작 80% 완성

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/main/PublicBoard.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/main/PublicBoard.js
@@ -13,6 +13,7 @@ import comments from '../icon/24px/comments.png';
 import sharing from '../icon/24px/sharing.png';
 import expand_more from '../icon/24px/expand-more.png';
 import emoticon_deactivation from '../icon/24px/emoticon-deactivation.png';
+import big_comment from '../icon/20px/bigcomment.png';
 
 function PublicBoard() {
     let [heart, setHeart] = useState(true); //좋아요 누름 확인
@@ -50,38 +51,57 @@ function PublicBoard() {
             </div>
             <div className={styles.contentArea}>{/* 본문 */}
                 <div className={styles.text}>
-                뉴비라 오늘 조합 검사 받았는데. 진짜 할거 많더라.<br/>
-                피드백 많이 해주셔서 원신 할거 겁나 많이 생김<br/>
-                우리 백출.. 잘 키워야지
+                    뉴비라 오늘 조합 검사 받았는데. 진짜 할거 많더라.<br />
+                    피드백 많이 해주셔서 원신 할거 겁나 많이 생김<br />
+                    우리 백출.. 잘 키워야지
                 </div>
-                <img src='https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/5367703706/B.jpg?675000000'/>
+                <img src='https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/5367703706/B.jpg?675000000' />
             </div>
-            <div className={styles.widthNav} style={{marginBottom:'0px'}}>{/* 하단 댓글,좋아요,공유 */}
+            <div className={styles.widthNav} style={{ marginBottom: '0px' }}>{/* 하단 댓글,좋아요,공유 */}
                 <div className={styles.commentsDiv}>
-{/*댓글창*/}    <div onClick={()=>{commentsON == false ? setCommentsON(true) : setCommentsON(false) }}>
-                    <img src={comments} /><div className={styles.comments}>123</div></div>
-{/*좋아요*/}    <div onClick={()=>{heart == false ? setHeart(true) : setHeart(false) }}>
-                   {heart === true ? <img src={heart_activation} /> :  <img src={heart_deactivation} />}
-                    <div className={styles.comments}>123123</div>
-                </div>
+                    {/*댓글창*/}    <div onClick={() => { commentsON == false ? setCommentsON(true) : setCommentsON(false) }}>
+                        <img src={comments} /><div className={styles.comments}>123</div></div>
+                    {/*좋아요*/}    <div onClick={() => { heart == false ? setHeart(true) : setHeart(false) }}>
+                        {heart === true ? <img src={heart_activation} /> : <img src={heart_deactivation} />}
+                        <div className={styles.comments}>123123</div>
+                    </div>
                 </div>
                 <img src={sharing} />
             </div>
-                {commentsON && <Comments/>}
+            {commentsON && <Comments />}
         </div>
     )
 }
 
-function Comments(){
-    return(
+function Comments() {
+    return (
         <div>
             <div className={styles.dashed} />{/* 회색줄 */}
-            <div className={styles.widthNav} style={{justifyContent:'start',cursor:'pointer'}}>정렬순서<img style={{marginLeft:'4px'}} src={expand_more}/></div>
-            <div className={styles.commentDiv}>
-                <textarea placeholder='댓글 달기'/>
+            <div className={styles.widthNav} style={{ justifyContent: 'start', cursor: 'pointer' }}>정렬순서<img style={{ marginLeft: '4px' }} src={expand_more} /></div>
+            <div className={styles.commentDiv}>{/* 댓글 달기 */}
+                <textarea placeholder='댓글 달기' />
                 <div className={styles.commentNav}>
-                <img src={emoticon_deactivation}/>
-                <div>0/200<button>등록</button></div>
+                    <img style={{ cursor: 'pointer' }} src={emoticon_deactivation} />
+                    <div>0/200<button>등록</button></div>
+                </div>
+            </div>
+            {/* 댓글 */}
+            <div className={styles.list}>
+                <div className={styles.listNav}>
+                    <div className={styles.listName}>닉네임<div className={styles.time}>4시간</div></div>
+                    <img style={{cursor:'pointer'}} src={more} /> {/* 신고삭제 모달 연결 해야함 */}
+                </div>
+                <div className={styles.listContent}>진짜ㅠ 너무 걱정했는데 잘 됬더라구요ㅠ</div>
+            </div>
+            {/* 대댓글 */}
+            <div className={styles.bigComments}>
+            <img className={styles.BcImg} src={big_comment} /> 
+                <div className={styles.list}>
+                    <div className={styles.listNav}>
+                        <div className={styles.listName}>닉네임<div className={styles.time}>4시간</div></div>
+                        <img src={more} /> {/* 신고삭제 모달 연결 해야함 */}
+                    </div>
+                    <div className={styles.listContent}>어쩌구 저쩌구 이래구 저래구 ^^</div>
                 </div>
             </div>
         </div>
