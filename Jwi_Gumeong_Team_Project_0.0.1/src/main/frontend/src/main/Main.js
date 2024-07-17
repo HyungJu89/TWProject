906c55268566a986dc6cc67d97a993d6ae3e2b56
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-07-17 14:40:04 +0900

    [최지은] > 로그인 후 CSS 정리 완료

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/main/Main.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/main/Main.js
@@ -8,10 +8,12 @@ import { useNavigate } from 'react-router-dom';
 import styles from './style/Main.module.css';
 import '../App.css';
 import PublicBoard from './PublicBoard.js'
+import edit from '../icon/20px/edit.png';
 
 function Main() {
     let navigate = useNavigate();
     let [topic, settopic] = useState(true);
+    let [loginOn, setLoginOn] = useState(false);
 
     const [channel, setChannel] = useState('');
     // 첫 번째 쿼리: 채널 정보를 가져오기.
@@ -57,15 +59,8 @@ function Main() {
                 <button style={{ width: '150px', height: '50px' }} on Click={() => { navigate('/channel/' + '123') }}>채널 확인 URL</button>
             </div>
             <div className={styles.rightDiv}>{/*유저 영역 */}
-                <div className={styles.userBefore}>
-                    스트리머 덕후들이 모이는 쥐구멍!
-                    <button style={{ width: '318px', height: '63px' }}>로그인</button>
-                    <div>
-                        <div className={styles.loginMenu} style={{ borderRight: '1px solid #999999' }}>회원가입</div>
-                        <div className={styles.loginMenu}>비밀번호 찾기</div>
-                    </div>
-                </div>
-                <div className={styles.recommendation}>
+                {loginOn == true ? <UserBefore/>:<UserAfter channel={channel}/>}
+                <div className={styles.recommendation}>{/* 추천 */}
                     <p style={{margin:'0px',marginLeft:'21px'}}>추천</p>
                     <div className={styles.list}>
                         <div className={styles.reChannel}><img src={channel.channelImageUrl} />{channel.channelName}</div>
@@ -97,6 +92,50 @@ function TopicBtn({ topic, settopic }) {
                 </div>}
         </div>
     )
+}
 
+function UserAfter({channel}){
+    return(
+    <div>
+    <div className={styles.userAfter} style={{borderRadius:'20px 20px 0px 0px'}}>
+        <div className={styles.userInfo}>
+            <div>
+                김박최조임권강이
+                <p>aaaaaa1234@naver.Com</p>
+            </div>
+            <button>로그아웃</button>
+        </div>
+        <button className={styles.myPage}>마이페이지</button>
+    </div>
+    <div className={styles.dashed} />{/* 회색줄 */}
+    <div className={styles.userAfter}style={{borderRadius:'00px 00px 20px 20px'}}>
+        <div className={styles.Bookmark}>즐겨찾기<img src={edit}/></div>
+        <div className={styles.recommendation}style={{marginTop:'0px', paddingTop:'20px'}}>{/* 추천 */}
+                    <div className={styles.list} style={{marginTop:'0px'}}>
+                        <div className={styles.reChannel}><img src={channel.channelImageUrl} />{channel.channelName}</div>
+                        <div className={styles.reChannel}><img src={channel.channelImageUrl} />{channel.channelName}</div>
+                        <div className={styles.reChannel}><img src={channel.channelImageUrl} />{channel.channelName}</div>
+                        <div className={styles.reChannel}><img src={channel.channelImageUrl} />{channel.channelName}</div>
+                        <div className={styles.reChannel}><img src={channel.channelImageUrl} />{channel.channelName}</div>
+                        <div className={styles.reChannel}><img src={channel.channelImageUrl} />{channel.channelName}</div>
+                        <div className={styles.reChannel}><img src={channel.channelImageUrl} />{channel.channelName}</div>
+                    </div>
+        </div>
+    </div>
+    </div>
+    )
+}
+
+function UserBefore(){
+    return(
+    <div className={styles.userBefore}>
+        스트리머 덕후들이 모이는 쥐구멍!
+        <button style={{ width: '318px', height: '63px' }}>로그인</button>
+        <div>
+            <div className={styles.loginMenu} style={{ borderRight: '1px solid #999999' }}>회원가입</div>
+            <div className={styles.loginMenu}>비밀번호 찾기</div>
+        </div>
+    </div>
+    )
 }
 export default Main;
