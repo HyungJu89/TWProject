6a13a7386125899878b6db745647d800fafcc8c3
ImPark <jaeyeol0311@gmail.com>
2024-07-18 20:21:18 +0900

    [임재열]24-07-18 즐겨찾기 버튼 컴포넌트화

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/search/Search.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/search/Search.js
@@ -1,3 +1,6 @@
+import BookmarkButton from '../channel/BookmarkButton.js';
+import style from './style/Search.module.css';
+import OpenChannel from '../icon/24px/Open-channel.png';
 function Search() {
 
 
@@ -6,26 +9,33 @@ function Search() {
 
 
     return (
-        <div className="searchMain">
-            <div className="searchBody">
-                <div className="searchBodyTop">
+        <div className={style.searchMain}>
+            <div className={style.searchBody}>
+                <div className={style.searchBodyTop}>
                     {/*상단 제목*/}
-                    <div className="searchTitle">토픽게시판</div>
-                    <div className="creactChannel">토픽게시판 만들기</div>
+                    <div className={style.searchTitle}>토픽 게시판</div>
+                    <div className={style.creactChannel}>
+                        <img className={style.creactChannelIcon} src={OpenChannel} />
+                        <div className={style.creactChannelText}>토픽 게시판 만들기</div>
+                    </div>
                 </div>
-                <div>
-                    {/*검색된 게시판 포문 돌려야함*/}
-                    <div className="searchBoard">
-                        <div className="searchChannelIcon">{/*게시판 아이콘 */}</div>
-                        <div className="searchChannelInfo">
-                            <div className="searchChannelTitle">{/*게시판 제목*/}</div>
-                            <div className="searchChannelmark">{/*게시판 팔로워,즐찾수*/}
-                                <div className="searchChannelFollower">팔로워</div>
-                                <div className="searchChannelBookmark">즐겨찾기</div>
+                <div className={style.searchChannelList}>                    {/*검색된 게시판 포문 돌려야함*/}
+                    <div className={style.searchChannel}>
+                        <div className={style.searchChannelIcon}>{/*게시판 아이콘 */}</div>
+                        <div className={style.searchChannelInfo}>
+                            <div className={style.searchChannelTitle}>{/*게시판 제목*/}제목</div>
+                            <div className={style.searchChannelmark}>{/*게시판 팔로워,즐찾수*/}
+                                <div className={style.searchChannelmarkText}>팔로워</div>
+                                <div className={style.searchChannelmarkCount}>9만9천</div>
+                                <div className={style.searchChannelmarkText}>즐겨찾기</div>
+                                <div className={style.searchChannelmarkCount}>9만9천</div>
                             </div>
                         </div>
-                        <div>{/*즐겨찾기 아이콘*/}</div>
+                        <div className={style.bookmark}>
+                            <BookmarkButton />
+                        </div>
                     </div>
+                    <div className={style.dashed} />{/* 회색줄 */}
                 </div>
                 <div>{/*페이징*/}</div>
             </div>
@@ -33,3 +43,5 @@ function Search() {
 
     )
 }
+
+export default Search;
