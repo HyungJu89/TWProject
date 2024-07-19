151a137d01bfcb1c241f93fb41c5f4d8307b0be6
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-07-19 17:30:52 +0900

    [최지은]2024-0719 > 이미지 팝업 구현 HyungJu89/TWProject#1

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/imgModal/imgModal.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/imgModal/imgModal.js
@@ -6,6 +6,8 @@ import '../App.css';
 import styles from './style/imgModal.module.css';
 import btn_image_Close from '../icon/btn/btn-image-Close.png';
 import btn_image_Close_a from '../icon/btn/btn-image-Close-a.png';
+import btn_content_close from '../icon/btn/btn-content-close.png';
+import btn_content_open from '../icon/btn/btn-content-open.png';
 import chevron_left_w from '../icon/40px/chevron-left-w.png';
 import chevron_right_w from '../icon/40px/chevron-right-w.png';
 import { useSelector, useDispatch } from 'react-redux';
@@ -14,6 +16,7 @@ import { openImgUiModalFalse } from '../slice/mainSlice';
 function ImgUi(){
     let disPatch = useDispatch();
     let [btnHover, setBtnHover] = useState(btn_image_Close);
+    let [content, setContent] = useState(false);
     return(
         <div className={styles.imgPopUp}>
             {/*닫기버튼*/}
@@ -22,20 +25,43 @@ function ImgUi(){
                 onMouseLeave={() => setBtnHover(btn_image_Close)}
                 className={styles.btnClose} src={btnHover} />
             {/*보이는 이미지*/}
+            <div className={styles.moveDiv}>
+            <div className={styles.moveDivWidth100}>
             <div className={styles.mainImgShow}>
-                <div className={styles.chevron_left_w}>
-                <img style={{marginRight:'0px'}} src={chevron_left_w}/>
+                <div className={styles.chevron}>
+                    <img style={{ marginRight: '0px' }} src={chevron_left_w} />
                 </div>
                 <div className={styles.mainImg}>
-                <img 
-                src='https://cdn.discordapp.com/attachments/1186577844755771465/1263754117571018873/image_2.png?ex=669b623d&is=669a10bd&hm=65cbc87a86a710cfeb01c4ca7ce74e82dbb838bfa0ff012eb4ef2973a3fe1971&' />
+                    <img
+                        src='https://cdn.discordapp.com/attachments/1186577844755771465/1263754117571018873/image_2.png?ex=669b623d&is=669a10bd&hm=65cbc87a86a710cfeb01c4ca7ce74e82dbb838bfa0ff012eb4ef2973a3fe1971&' />
                 </div>
-                <div className={styles.chevron_right_w}>
-                <img style={{marginLeft:'0px'}} src={chevron_right_w}/>
+                <div className={styles.chevron}>
+                    <img style={{ marginLeft: '0px' }} src={chevron_right_w} />
                 </div>
+            </div>
+            </div>
+            <div className={styles.mainContent}>
+                <div onClick={()=>{content== true ? setContent(false):setContent(true)}} className={styles.btn_content}>
+                {content == true ? <img src={btn_content_close}/>:<img src={btn_content_open}/>}
+                </div>
+               { content && <Content content={content} setContent={setContent}/>}
+            </div>
             </div>
         </div>
     )
 }
-
+function Content({setContent,content}){
+    return(
+        <div className={styles.fadeIn}>
+        <div className={styles.mainContent}>
+        <div onClick={()=>{content== true ? setContent(false):setContent(true)}} className={styles.btn_content}>
+        {content == true ? <img src={btn_content_close}/>:<img src={btn_content_open}/>}
+        </div>
+        <div className={styles.mainArea}>
+            ㅇㅅㅇ
+        </div>
+        </div>
+        </div>
+    )
+}
 export default ImgUi;
