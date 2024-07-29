77be78de03b7568cf32cf2114c22cd2aed268f54
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-07-29 17:03:20 +0900

    [최지은]2024-07-29 > 더미 삭제 추가

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/java/com/jwi/work/user/dto/User.java
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/java/com/jwi/work/user/dto/User.java
@@ -1,12 +1,6 @@
 package com.jwi.work.user.dto;
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
