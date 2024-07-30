c1dbddfb7230a59948656948b76f27bc9d943389
DESKTOP-U7IVOBB\user <gimhyeongju89@gmail.com>
2024-07-30 18:32:24 +0900

    김형주 [2024-07-30]
    이메일 중복체크, 닉네임 중복체크, 이메일 유효성검사, 닉네임 유효성검사 구현

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/java/com/jwi/work/user/dto/User.java
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/java/com/jwi/work/user/dto/User.java
@@ -13,5 +13,4 @@ public class User {
 
     private String gender;
 
-
 }
--- Jwi_Gumeong_Team_Project_0.0.1/src/main/java/com/jwi/work/user/signUp/service/SignUpService.java
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/java/com/jwi/work/user/signUp/service/SignUpService.java
@@ -11,7 +11,28 @@ public class SignUpService {
     
 	@Autowired
 	UserMapper userMapper;
+	
+	//회원가입
     public void saveUser(User user) {
-    	
+    	userMapper.register(user);
     }
-}
+    
+    // 이메일 중복검사
+    public boolean emailTest(String email) {
+    	if(userMapper.emailTest(email) == 0) {
+    		return true;
+    	}else {
+    		return false;
+    	}
+    }
+    
+    // 닉네임 중복검사
+    public boolean nickNameTest(String nickName) {
+    	if(userMapper.nickNameTest(nickName) == 0) {
+    		return true;
+    	}else {
+    		return false;
+    	}
+    }
+    
+}	
