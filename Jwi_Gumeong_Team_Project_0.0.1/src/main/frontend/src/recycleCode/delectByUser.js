951bb500c9c37afb5c78333453434aede6a16a6f
ImPark <jaeyeol0311@gmail.com>
2024-09-12 19:13:16 +0900

    [임재열] 2024-09-12 > 게시글, 댓글, 대댓글 일반유저 삭제 동작 과정이 유사하여 recycleCode 에 컴포넌트 생성 HyungJu89/TWProject#10

---
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/recycleCode/delectByUser.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+export const delectByUser = async (referenceType,referenceKey) => {
+    let sessionIdJson = sessionStorage.getItem('sessionId');
+    let sessionId = JSON.parse(sessionIdJson)?.sessionId
+
+    if (!sessionId) {
+        console.log("로그인 안되어있음");
+        return;
+    }
+
+    let apiUrl = '';
+    const delectByUserDto = {
+        sessionId : sessionId,
+        referenceKey : referenceKey,
+    }
+
+
+    switch (referenceType) {
+        case 'post':
+            apiUrl = '/post/deleteByUser';
+            break;
+        case 'comment':
+            apiUrl = '/comment/deleteByUser';
+            break;
+        case 'reply':
+            apiUrl = '/reply/deleteByUser';
+            break;
+        default:
+            console.error("잘못된 referenceType:", referenceType);
+            return;
+}
+try {
+    // axios.post로 API 요청
+    const { data } = await axios.post(apiUrl, delectByUserDto);
+    return data;
+} catch (error) {
+    console.error(`Error deleting ${referenceType}:`, error);
+    throw new Error(`Failed to delete ${referenceType}`);
+}
+
+};
