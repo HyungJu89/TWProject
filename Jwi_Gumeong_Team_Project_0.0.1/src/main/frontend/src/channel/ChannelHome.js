e745c971732942a00dd8586514ad99631cd3a603
ImPark <jaeyeol0311@gmail.com>
2024-09-14 14:41:37 +0900

    [임재열] 2024-09-14 > 코드정리

--- Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/channel/ChannelHome.js
+++ Jwi_Gumeong_Team_Project_0.0.1/src/main/frontend/src/channel/ChannelHome.js
@@ -70,7 +70,6 @@ function ChannelHome() {
     const handleCheckChannel = async (channelId) => {
         try {
             const channel = await channelGet(channelId); // 비동기 호출
-            console.log(channel.info);
             
             if (!channel.success) {
                 setModalContent('생성되지 않은 게시판입니다.');
